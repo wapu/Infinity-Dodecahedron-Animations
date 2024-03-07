6ab200934220b43e640960ae8ef3a8b7c7f2a4a0
Jakob Kruse <jakob.kruse@iwr.uni-heidelberg.de>
2024-03-07 14:50:07 +0100

    Fixed typo and added Interrupt handling

--- main.py
+++ main.py
@@ -45,58 +45,61 @@ times = []
 
 # Main loop
 print('Entering main loop...')
-while not done:
-    # Measure time
-    t_prev = time()
+try:
+    while not done:
+        # Measure time
+        t_prev = time()
 
-    # # Handle keyboard events
-    # if len(key_queue) > 0:
-    #     key = key_queue.pop(0)
-    #     if key == keyboard.Key.esc:
-    #         done = True
-    #         break
-    #     elif key == keyboard.Key.left:
-    #         d.next_animation(prev=True)
-    #         times.clear()
-    #         print(f'prev animation: {d.animation.__class__.__name__}', flush=True)
-    #     elif key == keyboard.Key.right:
-    #         d.next_animation(prev=False)
-    #         times.clear()
-    #         print(f'next animation: {d.animation.__class__.__name__}', flush=True)
+        # # Handle keyboard events
+        # if len(key_queue) > 0:
+        #     key = key_queue.pop(0)
+        #     if key == keyboard.Key.esc:
+        #         done = True
+        #         break
+        #     elif key == keyboard.Key.left:
+        #         d.next_animation(prev=True)
+        #         times.clear()
+        #         print(f'prev animation: {d.animation.__class__.__name__}', flush=True)
+        #     elif key == keyboard.Key.right:
+        #         d.next_animation(prev=False)
+        #         times.clear()
+        #         print(f'next animation: {d.animation.__class__.__name__}', flush=True)
 
-    # Run animation
-    try:
-        d.animation.step(t_delta_ms)
-    except:
-        done = True
-        print('Error :(')
-        break
+        # Run animation
+        try:
+            d.animation.step(t_delta_ms)
+        except:
+            done = True
+            print('Error :(')
+            break
 
-    # Lower brightness iff it exceeds set ampere threshold
-    ampere = (np.sum(d.colors)/255) * 0.060 * BRIGHTNESS
-    if ampere > MAX_AMPERE:
-        pixels.brightness = BRIGHTNESS * MAX_AMPERE/ampere
-    else:
-        pixels.brightness = BRIGHTNESS
+        # Lower brightness iff it exceeds set ampere threshold
+        ampere = (np.sum(d.colors)/255) * 0.060 * BRIGHTNESS
+        if ampere > MAX_AMPERE:
+            pixels.brightness = BRIGHTNESS * MAX_AMPERE/ampere
+        else:
+            pixels.brightness = BRIGHTNESS
 
-    # Send data to LED strip
-    color = d.colors.astype(int)
-    for i in range(len(d.leds)):
-        pixels[i] = colors[i]
-    pixels.show()
+        # Send data to LED strip
+        colors = d.colors.astype(int)
+        for i in range(len(d.leds)):
+            pixels[i] = colors[i]
+        pixels.show()
 
-    # Measure time
-    t_delta_ms = (time() - t_prev) * 1000
+        # Measure time
+        t_delta_ms = (time() - t_prev) * 1000
 
-    # Don't go faster than set FPS
-    if t_delta_ms < MS_PER_FRAME:
-        sleep((MS_PER_FRAME - t_delta_ms)/1000)
+        # Don't go faster than set FPS
+        if t_delta_ms < MS_PER_FRAME:
+            sleep((MS_PER_FRAME - t_delta_ms)/1000)
 
-    # Console output
-    times.append(t_delta_ms)
-    if len(times) > 1:
-        print(f'{1000/np.mean(times[-100:][1:]):.0f} FPS  |  {ampere:.1f} A', flush=True)
-
-
-# Turn off LED strip
-pixels.fill((0,0,0))
+        # Console output
+        times.append(t_delta_ms)
+        if len(times) > 1:
+            print(f'{1000/np.mean(times[-100:][1:]):.0f} FPS  |  {ampere:.1f} A', flush=True)
+except Exception as e:
+    print(f'Interrupted by {e}')
+finally:
+    print('Turning off LEDs')
+    # Turn off LED strip
+    pixels.fill((0,0,0))
